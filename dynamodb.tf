8726dc54d9331351637e1e71defcc895beb16e01
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2024-11-28 08:46:45 -0800

    added tf files

--- dynamodb.tf
+++ dynamodb.tf
@@ -156,3 +156,93 @@ resource "aws_dynamodb_table" "payroc_card_refund" {
     Name = "payroccardrefund"
   }
 }
+resource "aws_dynamodb_table" "payroc_card_void" {
+  name         = "payroccardvoid"
+  billing_mode = "PAY_PER_REQUEST"
+
+  # Primary partition key
+  hash_key = "transaction_id"
+
+  # Define all the attributes
+  attribute {
+    name = "transaction_id"
+    type = "S" # String (Partition Key)
+  }
+
+  attribute {
+    name = "authorization_code"
+    type = "S" # String
+  }
+
+  attribute {
+    name = "card_last_four_digits"
+    type = "S" # String
+  }
+
+  attribute {
+    name = "card_product"
+    type = "S" # String
+  }
+
+  attribute {
+    name = "card_country_of_origin"
+    type = "N" # Number (since it's numeric)
+  }
+
+  attribute {
+    name = "expiry_date"
+    type = "S" # String
+  }
+
+  attribute {
+    name = "card_type"
+    type = "S" # String
+  }
+
+  # Optional Global Secondary Indexes (if specific query patterns require them)
+  global_secondary_index {
+    name            = "AuthorizationCodeIndex"
+    hash_key        = "authorization_code"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "CardCountryIndex"
+    hash_key        = "card_country_of_origin"
+    projection_type = "ALL"
+  }
+
+  # Additional Global Secondary Indexes for unused attributes
+  global_secondary_index {
+    name            = "CardLastFourDigitsIndex"
+    hash_key        = "card_last_four_digits"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "CardProductIndex"
+    hash_key        = "card_product"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "CardTypeIndex"
+    hash_key        = "card_type"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "ExpiryDateIndex"
+    hash_key        = "expiry_date"
+    projection_type = "ALL"
+  }
+
+  # Point-In-Time Recovery (enabled for data recovery)
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = {
+    Name = "payroccardvoid"
+  }
+}
